a735e7354ade6913d014f9b7e45170b8f2fc8e55
nitiponth <ntitiponth@outlook.com>
2021-04-18 18:42:30 +0700

    section 11 done!
    But not things  :angry:   ( :-1: for me!)

---
+++ practice-project/src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import AvailablesMeals from "./AvailableMeals";
+import MealsSummary from "./MealsSummary";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailablesMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;
